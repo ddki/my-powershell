d87dae1e5a05f48debf6a03ed2a2347b4dc9a93d
ddki <ddki2068@gmail.com>
2023-09-22 01:16:33 +0800

    🎉 feat:init

--- src/core/job.rs
+++ src/core/job.rs
@@ -1,6 +1,12 @@
-use std::{collections::HashMap, ops::Add, thread, time::Duration};
+use std::{
+    collections::{hash_map::RandomState, HashMap},
+    env,
+    io::{self, BufRead},
+    process::{Command, Stdio},
+};
 
 use console::style;
+use dialoguer::{theme::ColorfulTheme, Input};
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use serde::{Deserialize, Serialize};
 
@@ -22,6 +28,14 @@ pub struct CommandInfo {
     description: Option<String>,
     command: Option<String>,
     args: Option<HashMap<String, String>>,
+    full_command: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct CommandArgInfo {
+    need_input: Option<bool>,
+    value: Option<String>,
+    label: Option<String>,
 }
 
 impl Job {
@@ -48,12 +62,19 @@ impl Job {
                 let pb = mp.add(ProgressBar::new(commands.len().try_into().unwrap()));
                 pb.set_style(spinner_style.clone());
                 pb.set_prefix("[pre_command]");
+                let mut success_flag = true;
                 for command in commands {
-                    command.execute();
-                    pb.set_message(command.full_command());
+                    if command.clone().execute().is_err() {
+                        success_flag = false;
+                    }
+                    pb.set_message(command.clone().full_command_str());
                     pb.inc(1);
                 }
-                pb.finish_with_message("✔️ pre_command finished！");
+                if success_flag {
+                    pb.finish_with_message("✔️ pre_command finished！");
+                } else {
+                    pb.finish_with_message("❌ pre_command finished！");
+                }
                 let _ = mp.clear();
             }
             None => {}
@@ -65,12 +86,19 @@ impl Job {
                 let pb = mp.add(ProgressBar::new(installs.len().try_into().unwrap()));
                 pb.set_style(spinner_style.clone());
                 pb.set_prefix("[install_command]");
+                let mut success_flag = true;
                 for install in installs {
-                    install.execute();
-                    pb.set_message(install.full_command());
+                    if install.clone().execute().is_err() {
+                        success_flag = false;
+                    }
+                    pb.set_message(install.clone().full_command_str());
                     pb.inc(1);
                 }
-                pb.finish_with_message("✔️ install_command finished！");
+                if success_flag {
+                    pb.finish_with_message("✔️ install_command finished！");
+                } else {
+                    pb.finish_with_message("❌ install_command finished！");
+                }
                 let _ = mp.clear();
             }
             None => {}
@@ -82,12 +110,20 @@ impl Job {
                 let pb = mp.add(ProgressBar::new(commands.len().try_into().unwrap()));
                 pb.set_style(spinner_style.clone());
                 pb.set_prefix("[post_command]");
+                let mut success_flag = true;
                 for command in commands {
-                    command.execute();
-                    pb.set_message(command.full_command());
+                    if command.clone().execute().is_err() {
+                        success_flag = false;
+                    }
+                    pb.set_message(command.clone().full_command_str());
                     pb.inc(1);
                 }
-                pb.finish_with_message("✔️ post_command finished！");
+                if success_flag {
+                    pb.finish_with_message("✔️ post_command finished！");
+                } else {
+                    pb.finish_with_message("❌ post_command finished！");
+                }
+
                 let _ = mp.clear();
             }
             None => {}
@@ -120,26 +156,107 @@ impl Job {
 }
 
 impl CommandInfo {
-    pub fn execute(&self) {
-        let _commands = self.command.as_ref().unwrap_or(&String::default()).clone();
-        thread::sleep(Duration::from_millis(2000));
+    pub fn execute(&mut self) -> Result<String, &'static str> {
+        // 构建tui，输入参数值
+        self.args_tui();
+        // 获取参数
+        let args_str = self.args_str();
+
+        match self.clone().command {
+            Some(command_str) => {
+                if let Ok(current_dir) = env::current_dir() {
+                    let current_dir_str = current_dir.to_string_lossy().into_owned();
+                    // 设置全命令
+                    let full_command = format!("{} {}", &command_str, &args_str);
+                    if cfg!(debug_assertions) {
+                        println!("full_command: {}", full_command);
+                    }
+                    self.set_full_command(Some(full_command));
+
+                    // 创建执行命令
+                    let mut command = Command::new(&command_str);
+                    command.current_dir(&current_dir_str);
+                    let args_flag = !args_str.is_empty();
+                    if args_flag {
+                        command.arg(&args_str);
+                    }
+
+                    // powershell 脚本
+                    if command_str.contains(".ps1") {
+                        command = Command::new("powershell");
+                        command.current_dir(&current_dir_str);
+                        command
+                            .arg("-ExecutionPolicy")
+                            .arg("Bypass")
+                            .arg("-File")
+                            .arg(&command_str);
+                        if args_flag {
+                            command.arg(&command_str).arg(&args_str).stdout(Stdio::piped());
+                        }
+                    }
+
+                    let mut output =
+                        command.stdout(Stdio::piped()).spawn().expect("执行命令发生错误！");
+
+                    if let Some(stdout) = output.stdout.take() {
+                        let reader = io::BufReader::new(stdout);
+                        for line in reader.lines() {
+                            if let Ok(line) = line {
+                                println!("{}", line);
+                            }
+                        }
+                    }
+
+                    let status = output.wait().expect("failed to wait for child process");
+                    if status.success() {
+                        println!("😃 命令执行成功！");
+                        Ok(String::from("😃 命令执行成功！"))
+                    } else {
+                        println!("😢 命令执行失败！");
+                        Err("😢 命令执行失败！")
+                    }
+                } else {
+                    println!("😢 获取当前程序目录失败！");
+                    Err("😢 获取当前程序目录失败！")
+                }
+            }
+            None => Ok(String::default()),
+        }
     }
 
-    pub fn full_command(&self) -> String {
-        self.command.as_ref().unwrap_or(&String::default()).clone()
+    // 构建tui，输入参数值
+    pub fn args_tui(&mut self) {
+        match &self.args {
+            Some(args) => {
+                let mut input_args = HashMap::new();
+                for (key, value) in args {
+                    let inpur_value: String = Input::with_theme(&ColorfulTheme::default())
+                        .with_prompt(format!("请输入{}：", &value))
+                        .allow_empty(true)
+                        .interact_text()
+                        .unwrap();
+                    input_args.insert(key.to_string(), inpur_value);
+                }
+                println!("{:#?} {:?}", self, input_args);
+                if !input_args.is_empty() {
+                    self.set_args(Some(input_args));
+                }
+            }
+            None => {}
+        }
     }
 
-    fn args_str(&self) -> String {
+    fn args_str(&mut self) -> String {
         return match &self.args {
             Some(args) => {
                 let mut args_str = String::new();
                 let start = args.get("start").unwrap_or(&String::default()).clone();
+                let joiner = args.get("joiner").unwrap_or(&String::from(" ")).clone();
                 for (key, value) in args.iter() {
-                    if key == "start" {
+                    if key == "start" || key == "joiner" || key.is_empty() || value.is_empty() {
                         continue;
                     }
-                    // todo tui输入
-                    args_str += &format!("{}{} ", start, value);
+                    args_str += &format!("{}{}{}{} ", start, key, joiner, value);
                 }
                 return args_str;
             }
@@ -153,5 +270,21 @@ impl CommandInfo {
 
     pub fn to_json(&self) -> String {
         serde_json::to_string_pretty(self).unwrap_or_default()
+    }
+
+    pub fn set_args(&mut self, args: Option<HashMap<String, String>>) {
+        self.args = args;
+    }
+
+    pub fn args(&self) -> Option<&HashMap<String, String, RandomState>> {
+        self.args.as_ref()
+    }
+
+    pub fn set_full_command(&mut self, full_command: Option<String>) {
+        self.full_command = full_command;
+    }
+
+    pub fn full_command_str(&mut self) -> String {
+        self.full_command.as_ref().unwrap_or(&String::default()).to_string()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 use core::workflow::Workflow;
 use std::{fs::File, io::ErrorKind};
 
-use serde::{Deserialize, Serialize};
 use clap::Parser;
 use rust_embed::RustEmbed;
+use serde::{Deserialize, Serialize};
 
-pub mod core;
 pub mod constant;
+pub mod core;
 pub mod util;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Config {
-    workflow: Option<Workflow>
+    workflow: Option<Workflow>,
+}
+
+impl Config {
+    pub fn new() -> Config {
+        Self { workflow: None }
+    }
 }
 
 #[derive(RustEmbed)]
@@ -25,10 +31,13 @@ struct Asset;
 #[command(version = "0.0.1")]
 #[command(about = "My PowerShell", long_about = None)]
 pub struct Args {
-    #[arg(long, default_value = "my-powershell.toml", help = "配置文件，支持toml、json。默认：my-powershell.toml")]
+    #[arg(
+        long,
+        default_value = "my-powershell.json",
+        help = "配置文件，支持toml、json。默认：my-powershell.toml"
+    )]
     config: String,
 }
-
 
 fn main() {
     let args = Args::parse();
@@ -42,24 +51,28 @@ fn main() {
                     // 加载默认配置文件
                     let default_config_template = Asset::get(config_file_name).unwrap();
                     // 配置文件不存在，创建新的默认配置文件
-                    std::fs::write(config_path, std::str::from_utf8(default_config_template.data.as_ref()).unwrap()).expect("创建默认配置文件出错！");
+                    std::fs::write(
+                        config_file_name,
+                        std::str::from_utf8(default_config_template.data.as_ref()).unwrap(),
+                    )
+                    .expect("创建默认配置文件出错！");
                 }
                 // 返回配置文件
                 File::open(config_path).unwrap()
-            },
+            }
             _ => {
                 panic!("无法打开配置文件：{:?}", error);
             }
-        }
+        },
     };
 
     let config_str = std::fs::read_to_string(config_path).expect("打开配置文件出错！");
-    let mut config: Config = Config { workflow: None };
+    let mut config: Config = Config::new();
     if let Some(suffix) = config_path.split(".").last() {
         match suffix {
             "json" => config = serde_json::from_str(&config_str).unwrap(),
             "toml" => config = toml::from_str(&config_str).unwrap(),
-            _ => panic!("不支持 {} 格式的配置文件", suffix)
+            _ => panic!("不支持 {} 格式的配置文件", suffix),
         }
     }
     // debug 模式打印日志
